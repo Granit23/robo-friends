b8a7a00524e40e3a90517d648878d60bfc5e077d
Granit23 <granitarifi.23@gmail.com>
2019-03-20 07:46:09 -0400

    Adds Redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
 import './index.css';
 import App from './containers/App'
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
+import { searchRobots } from './reducers';
 // import { robots } from './robots'
+const store = createStore(searchRobots);
 
 ReactDOM.render(
-  (<App />)
-  , document.getElementById('root'));
-
+  <Provider store={store}>
+    <App />
+  </Provider>, document.getElementById('root'));
 serviceWorker.unregister();
+ 
